e5dca5a62246efd11c8d8eb9d4bf10a1beea27a9
tabithaz <140666866+tabithaz@users.noreply.github.com>
2024-08-11 17:53:58 -0700

    Update data.py

--- data.py
+++ data.py
@@ -1,20 +1,4 @@
-""" A neural chatbot using sequence to sequence model with
-attentional decoder. 
 
-This is based on Google Translate Tensorflow model 
-https://github.com/tensorflow/models/blob/master/tutorials/rnn/translate/
-
-Sequence to sequence model by Cho et al.(2014)
-
-Created by Chip Huyen (chiphuyen@cs.stanford.edu)
-CS20: "TensorFlow for Deep Learning Research"
-cs20.stanford.edu
-
-This file contains the code to do the pre-processing for the
-Cornell Movie-Dialogs Corpus.
-
-See readme.md for instruction on how to run the starter code.
-"""
 import os
 import random
 import re
